7d355c8555918ef383781008f8de3c0a647d6612
Swapnil Sahu <sahu.swapnil496@gmail.com>
2025-02-10 16:01:44 +0530

    display name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {React, useState} from 'react';
 
 function App() {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const handleFname = (e) => {
+    setFname(e.target.value)
+  }
+
+  const handleLname = (e) => {
+    setLname(e.target.value);
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setFullName(`${fname} ${lname}`);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Full Name Display</h1>
+      <form onSubmit={(e) => handleClick(e)}> 
+  <label for="fname">First name: </label>
+  <input type="text" id="fname" name="fname" required={true} value={fname} onChange={(e) => handleFname(e)}/><br/><br/>
+  <label for="lname">Last name: </label>
+  <input type="text" id="lname" name="lname" required={true} value={lname} onChange={(e) => handleLname(e)}/><br/><br/>
+  <button >Submit</button>
+</form> 
+{fullName ? <p>Full Name: {fullName}</p> : () => {return;}} 
     </div>
   );
 }
